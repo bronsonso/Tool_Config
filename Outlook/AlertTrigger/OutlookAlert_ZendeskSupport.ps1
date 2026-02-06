74cfcf6d43a19cb230acaba27799ac3787886e06
Bronson So <bronson.so@hkecl.net>
2026-02-06 14:13:48 +0800

    move function Convert-TimestampToGMT8 to top

--- Outlook/AlertTrigger/OutlookAlert_ZendeskSupport.ps1
+++ Outlook/AlertTrigger/OutlookAlert_ZendeskSupport.ps1
@@ -1,4 +1,26 @@
+# ******************************** * COMMON FUNCTIONS * ********************************
 
+# * Function to Convert Timestamp to GMT+8 *
+function Convert-TimestampToGMT8 {
+    param(
+        [string]$InputText
+    )
+    
+    $convertedText = [regex]::Replace($InputText, 'Received:\s*([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{2}:[0-9]{2})', {
+        param($match)
+        
+        $timestampString = $match.Groups[1].Value
+        $utcTime = [DateTimeOffset]::Parse($timestampString)
+        $gmtPlus8Time = $utcTime.ToOffset([TimeSpan]::FromHours(8))
+        $formattedTime = $gmtPlus8Time.ToString("yyyy-MM-dd HH:mm:ss")
+
+        return "Received: $formattedTime"
+    })
+    
+    return $convertedText
+}
+
+# ******************************** * MAIN SCRIPT * ********************************
 $triggerDir = "$env:USERPROFILE\OneDrive - Hong Kong Education City Limited\EdCity\Misc\AlertTrigger"
 $processedDir = "$triggerDir\processed"
 
@@ -14,30 +36,9 @@ foreach ($file in $files) {
 
     $content = Convert-TimestampToGMT8 -InputText $content
 
-    powershell.exe -ExecutionPolicy Bypass -File "C:\Users\bronson.so\Documents\EdCity\Useful Documents\Scripts\Common\PersistentPopup.ps1" `
+    powershell.exe -ExecutionPolicy Bypass -File "C:\Users\bronson.so\Documents\EdCity\Repository\Tool\Config\Outlook\Common\PersistentPopup.ps1" `
         -Title "Important Mail (Zendesk Support)" `
         -Message $content
 
     Move-Item $file.FullName -Destination $processedDir -Force
-}
-# ******************************** * COMMON FUNCTIONS * ********************************
-
-# * Function to Convert Timestamp to GMT+8 *
-function Convert-TimestampToGMT8 {
-    param(
-        [string]$InputText
-    )
-    
-    $convertedText = [regex]::Replace($InputText, 'Received:\s*([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{2}:[0-9]{2})', {
-        param($match)
-        
-        $timestampString = $match.Groups[1].Value
-        $utcTime = [DateTimeOffset]::Parse($timestampString)
-        $gmtPlus8Time = $utcTime.ToOffset([TimeSpan]::FromHours(8))
-        $formattedTime = $gmtPlus8Time.ToString("yyyy-MM-dd HH:mm:ss 'GMT+8'")
-        
-        return "Received: $formattedTime"
-    })
-    
-    return $convertedText
 }
--- Outlook/AlertTrigger/OutlookAlert_ZendeskSupport.vbs
+++ Outlook/AlertTrigger/OutlookAlert_ZendeskSupport.vbs
@@ -1,4 +1,4 @@
 Set objShell = CreateObject("WScript.Shell")
-objShell.Run "powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File ""C:\Users\bronson.so\Documents\EdCity\Useful Documents\Scripts\OutlookTrigger\AlertFromOutlookZendeskEmail.ps1""", 0, True
+objShell.Run "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File ""C:\Users\bronson.so\Documents\EdCity\Repository\Tool\Config\Outlook\AlertTrigger\OutlookAlert_ZendeskSupport.ps1""", 0, True
 
 
